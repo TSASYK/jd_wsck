5a256177db777de25e9e8084cc184688a128a8d9
Sky Lan <lanskytian@gmail.com>
2023-11-21 03:54:37 +0000

    init

---
+++ loon/client.js
@@ -0,0 +1,17 @@
+const req = $request;
+
+console.log('This is a log');
+console.log(req.method);
+if (req.method != 'OPTIONS' && req.headers && req.headers.Host == 'api.m.jd.com') {
+    // const cookieVal = "wskey=AAJlSvaNAECkCJQWndDTdb618CzpBqt-5g2pa5GahrXZEC8BIM4dKbSR0aT3cu9kWqYgefe1xFHAZ5FqSMREbOYM-RdfOUEg;whwswswws=~AAsQhybeLEaA6kRGifkyr6MOU36X5hVWeFDTufwAAAApsYW5za3l0aWFu"
+    const cookieVal = (req.headers['Cookie'] || req.headers['cookie'] || '');
+    const ckItems = cookieVal.match(/wskey=(.+?);/);
+    console.log(ckItems);
+    if (ckItems && ckItems.length == 2) {
+        $httpClient.get(`http://192.168.50.3:15307/jd?wsck=${ckItems[1]}`, function (error, response, data) {
+            console.log(data);
+        });
+    }
+} else {
+    throw new Error("req wrong");
+}
---
+++ server.js
@@ -0,0 +1,122 @@
+const request = require('request');
+const util = require('util');
+const child_process = require(`child_process`);
+
+const QL_ENV_HTTP_URL = 'http://127.0.0.1:5600/api/envs'
+const QL_ENV_ENABLE_HTTP_URL = 'http://127.0.0.1:5600/api/envs/enable'
+
+const requestPromise = util.promisify(request);
+const exec = util.promisify(child_process.exec);
+
+function getQlCookie() {
+    var fs = require('fs'),
+        path = require('path'),
+        filePath = path.join('/ql/data/config', 'auth.json');
+    var json = fs.readFileSync(filePath, { encoding: 'utf-8' });
+    var author = JSON.parse(json);
+    return author['token'];
+}
+
+async function getJdWsck(token) {
+    let result = await requestPromise({
+        url: QL_ENV_HTTP_URL,
+        method: 'GET',
+        json: true,
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
+        qs: {
+            'searchValue': 'JD_WSCK',
+        }
+    });
+    return result.body.data;
+}
+
+async function updateJdWsck(token, id, wsck) {
+    let result = await requestPromise({
+        url: QL_ENV_HTTP_URL,
+        method: 'PUT',
+        json: true,
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
+        body: {
+            'value': `pin=lanskytian;wskey=${wsck};`,
+            'name': 'JD_WSCK',
+            'id': id
+        },
+    });
+    console.log(result.body)
+    return result.body;
+}
+
+async function enableJdWsck(token, id) {
+    let result = await requestPromise({
+        url: QL_ENV_ENABLE_HTTP_URL,
+        method: 'PUT',
+        json: true,
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
+        body: [id],
+    });
+    console.log(result.body)
+    return result.body;
+}
+
+
+async function addJdWsck(token, wsck) {
+    let result = await requestPromise({
+        url: QL_ENV_HTTP_URL,
+        method: 'POST',
+        json: true,
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
+        body: [{
+            'value': `pin=lanskytian;wskey=${wsck};`,
+            'name': 'JD_WSCK',
+        }],
+    });
+    console.log(result.body)
+    return result.body;
+}
+
+// var token = getQlCookie();
+// getJdWsck(token).then((data) => {
+//     if(data && data[0]) {
+//         console.log(data[0]['id'])
+//         updateJdWsck(token, data[0]['id'], 'asdasdasdasd');
+//         console.log('Update wsck wsck')
+//     } else {
+//         addJdWsck(token, 'wsck');
+//         console.log('add wsck wsck')
+//     }
+// })
+
+const express = require('express');
+var app = express();
+app.get('/jd', function (req, res) {
+    if (!req.query || !req.query.wsck) {
+        return res.status(500).json({ message: 'wrong' });
+    }
+    var token = getQlCookie();
+    getJdWsck(token).then((envList) => {
+        if (envList && envList[0]) {
+            updateJdWsck(token, envList[0]['id'], req.query.wsck).then((result) => {
+                enableJdWsck(token, envList[0]['id']).then((enable_data) => {
+                    console.log(`after enable wsck, update cookie.`);
+                    exec(`task shufflewzc_faker2_main/jd_wsck.py`);
+                });
+                return res.status(200).json({ message: `Update wsck succeed: ${JSON.stringify(result.data)}` });
+            });
+        } else {
+            addJdWsck(token, req.query.wsck).then((result) => {
+                exec(`task shufflewzc_faker2_main/jd_wsck.py`);
+                return res.status(200).json({ message: `add wsck succeed: ${JSON.stringify(result.data)}` });
+            });
+        }
+    })
+});
+
+app.listen(5701);
